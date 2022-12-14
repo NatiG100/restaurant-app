c89a83bda31d1ae4ec747f743762cf6552e9eb1a
NatiG100 <natnaelgashu2022@gmail.com>
2022-12-14 10:11:46 -0800

    create search

---
+++ components/Search/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef, useState } from "react";
+import {HiOutlineMagnifyingGlass as SearchIcon} from 'react-icons/hi2';
+
+export const enum SearchSize {
+    sm=64,
+    md=80,
+    lg=96,
+    full=100
+}
+interface SearchParam {
+    size?: SearchSize,
+    onChange: (event: React.ChangeEvent<HTMLInputElement>)=>void,
+    value: String
+}
+export default function Search({size=SearchSize.lg ,onChange} : SearchParam){
+    const [isFocus, setisfocus] = useState<boolean>(true);
+    const ref = useRef<HTMLInputElement>(null);
+    useEffect(()=>{
+        if(ref.current){
+            setisfocus(ref.current===document.activeElement);
+        }
+    },[])
+    const handleFocus = ()=>{
+        setisfocus(true);
+    }
+    const handleBlur = ()=>{
+        setisfocus(false);
+    }
+    const handleClick = ()=>{
+        ref.current?.focus();
+    }
+    return(
+        <div className="h-10 grid grid-cols-mx1fr items-center relative">
+            <SearchIcon 
+                size={37} className={`pointer-events-none p-2 absolute t-0 r-0 z-10 ${isFocus?"text-slate-600":"text-slate-400"} stroke-2`}
+            />
+            <input 
+                className={`bg-transparent absolute t-0 r-0 outline-0 h-full w-full text-lg pl-9
+                    w-${size===SearchSize.full?"full":size} 
+                    bg-slate-50 border-slate-300 border focus:border-slate-400 focus:bg-slate-200
+                    rounded text-slate-500 focus:text-slate-800 font-normal`} 
+                placeholder="Search"
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                ref={ref}
+            />
+        </div>
+    );
+}
