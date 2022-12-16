6ce0ee6010fae8ac2e9580202fda71f47ae22ead
NatiGK <natnaelgashu2022@gmail.com>
2022-12-16 12:32:09 +0000

    fix stat cmpnt

--- components/chart/Stat/index.tsx
+++ components/chart/Stat/index.tsx
@@ -1,3 +1,5 @@
+import {AiOutlineArrowDown,AiOutlineArrowUp} from 'react-icons/ai';
+
 export const enum notation {
     K = 1000,
     M = 1000000,
@@ -9,10 +11,11 @@ interface statInterface {
     delta: number,
     icon: React.ReactNode,
     value: number,
-    postfix: string,
+    postfix?: string,
     precision: number,
     notation:notation
 }
+
 
 export default function Stat (props:statInterface) {
     return(
@@ -20,18 +23,25 @@ export default function Stat (props:statInterface) {
             <div className="bg-indigo-700 p-2 rounded-full h-max w-max">
                 {props.icon}
             </div>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col justify-center">
                 <p className="text-lg text-gray-700 font-semibold">{props.title}</p>
                 <p className="text-2xl text-yellow-600 font-bold">
-                    {(props.value/props.notation).toFixed(props.precision)}
+                    {props.notation===notation.none?props.value.toFixed(props.precision):(props.value/props.notation).toFixed(props.precision)}
                     {props.notation===notation.K?"K":props.notation===notation.M?"M":props.notation===notation.B?"M":""}
-                     {" " + props.postfix}
+                     {" " + ( props.postfix?props.postfix:"")}
                 </p>
             </div>
-            <p className={`text-base absolute top-4 right-6 ${props.delta<0?"text-red-600":"text-green-600"}`}>
-
-                {props.delta<0?-1*props.delta:props.delta}%
-            </p>
+            <div className='flex items-center justify-center h-max w-max absolute top-4 right-6'>
+                {props.delta>0?
+                        <AiOutlineArrowUp className='text-green-600'/>
+                    :props.delta<0?
+                        <AiOutlineArrowDown className='text-red-600'/>
+                    :null
+                }
+                <p className={`text-base  ${props.delta<0?"text-red-600":"text-green-600"}`}>
+                    {props.delta<0?-1*props.delta:props.delta}%
+                </p>
+            </div>
         </div>
     );
 }
