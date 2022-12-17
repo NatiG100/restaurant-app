555df05243ec35c2ddf162e4d7f94de90a3b52e3
NatiG100 <natnaelgashu2022@gmail.com>
2022-12-17 11:18:12 -0800

    create graph container component

--- components/chart/ChartContainer/index.tsx
+++ components/chart/ChartContainer/index.tsx
@@ -1,4 +1,27 @@
+interface ChartContainerInterface {
+    title: string,
+    hasFilter: boolean,
+    filterItems: {key:number, text:string}[],
+    chart: React.ReactNode,
+    children: React.ReactNode,
+}
 
-export default function ChartContainer(){
-
+export default function ChartContainer(props:ChartContainerInterface){
+    return(
+        <div className="p-5 rounded-md bg-white grid grid-rows-maxmax w-full shadow-custom-lg">
+            <div className="w-full flex items-center justify-between">
+                <div className="flex items-center gap-4">
+                    <div className="rounded-tl-sm rounded-bl-sm w-4 h-20 bg-green-200"></div>
+                    <p className="text-gray-800">Sales</p>
+                </div>
+                <select>
+                    {props.filterItems.map((items)=>(
+                            <option key={items.key}>{items.text}</option>
+                        ))
+                    }
+                </select>
+            </div>
+            {props.children}
+        </div>
+    );
 }
