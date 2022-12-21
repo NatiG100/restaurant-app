0028af28375b71b48194d07a7594cb2a0e5cf377
NatiGK <natnaelgashu2022@gmail.com>
2022-12-21 12:32:06 +0000

    test columns and cell style

--- pages/orders.tsx
+++ pages/orders.tsx
@@ -4,6 +4,7 @@ import This from "ag-grid-community"
 import { AgGridReact } from "ag-grid-react";
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { ColumnFormat } from "ag-grid-community/dist/lib/main";
 
 export enum status {
     pending,
@@ -20,115 +21,39 @@ export interface OrderInterface{
 }
 
 
-const gridStyle = {minHeight: 550}
-const rows:OrderInterface[] = [
-    {
-        _id:"asdjkfjasfjksddk",
-        date:"15/10/2012",
-        totalCost:150,
-        timeElapsed:"1h 20m",
-        status:status.pending
-    },
-    {
-        _id:"dfghsjmxcxvrtssr",
-        date:"15/10/2012",
-        totalCost:120,
-        timeElapsed:"1h 20m",
-        status:status.pending
-    },
-    {
-        _id:"nsreygjdfmlfgou",
-        date:"15/10/2012",
-        totalCost:98,
-        timeElapsed:"1h 20m",
-        status:status.pending
-    },
-    {
-        _id:"batrehnbsfsfgfd",
-        date:"15/10/2012",
-        totalCost:240,
-        timeElapsed:"1h 20m",
-        status:status.pending
-    },
-    {
-        _id:"lpoiunmxijudjwop",
-        date:"15/10/2012",
-        totalCost:170,
-        timeElapsed:"1h 20m",
-        status:status.pending
-    },
-    {
-        _id:"poiuijkadfcnvkal",
-        date:"15/10/2012",
-        totalCost:99,
-        timeElapsed:"1h 20m",
-        status:status.pending
-    },
-]
 
-const columns: readonly Column<OrderInterface,unknown>[] = [
-    {
-        key:"_id",
-        name:"ID",
-        width:20,
-        cellClass:(row)=>`inline`,
-    },
-    {
-        key:"date",
-        name:"Date",
-        width:10,
-        cellClass:"inline",
-    },
-    {
-        key:"totalCost",
-        name:"Total Cost",
-        width:10,
-        cellClass:"inline",
-    },
-    {
-        key:"timeElapsed",
-        name:"Time Elapsed",
-        width:10,
-        cellClass:"inline",
-    },
-    {
-        key:"status",
-        name:"Status",
-        width:10,
-        cellClass:"inline",
-        // :({value}:{value:status})=>{
-        //     return(
-        //         <p
-        //             className={`text-${
-        //                 value===status.pending?"yellow":
-        //                 value===status.started?"indigo":
-        //                 value===status.served?"green":
-        //                 value===status.canceled?"red":
-        //                 "gray"
-        //             }`}
-        //         >{value}</p>
-        //     );
-        // }
-    }
-];
-
+const statusColumnClass = (param:any)=>(
+    param?.value==="pending"?"text-yellow-600 bg-yellow-50":
+    param?.value==="started"?"text-indigo-600 bg-indigo-50":
+    param?.value==="served"?"text-green-600 bg-green-50":
+    param?.value==="canceled"?"text-red-600 bg-red-50":
+    "text-gray-600"
+);
 export default function Orders(){
     const [rowData] = useState([
-        {make: "Toyota", model: "Celica", price: 35000},
-        {make: "Ford", model: "Mondeo", price: 32000},
-        {make: "Porsche", model: "Boxster", price: 72000}
+        {_id:"nakdjfjeial",date: "10/10/2015", totalCost: 4000, timeElapsed: "1hr 10min",tableNumber:"A10",status:"pending"},
+        {_id:"nsdknvfkkcd",date: "10/10/2015", totalCost: 4000, timeElapsed: "1hr 10min",tableNumber:"A10",status:"pending"},
+        {_id:"alksdfj,dfd",date: "10/10/2015", totalCost: 4000, timeElapsed: "1hr 10min",tableNumber:"A10",status:"started"},
+        {_id:"ueiopaisdfx",date: "10/10/2015", totalCost: 4000, timeElapsed: "1hr 10min",tableNumber:"A10",status:"canceled"},
+        {_id:"nalskjsdoto",date: "10/10/2015", totalCost: 4000, timeElapsed: "1hr 10min",tableNumber:"A10",status:"served"},
+        {_id:"abegkielmdf",date: "10/10/2015", totalCost: 4000, timeElapsed: "1hr 10min",tableNumber:"A10",status:"served"},
     ]);
     const [columnDefs] = useState([
-        { field: 'make' },
-        { field: 'model' },
-        { field: 'price' }
+        { field: '_id',headerName:"ID" },
+        { field: 'date',headerName:"Date", type: ['dateColumn', 'nonEditableColumn'] },
+        { field: 'totalCost',headerName:"Total Cost" },
+        { field: 'timeElapsed',headerName:"Time Elapsed" },
+        { field: 'tableNumber',headerName:"Table Number" },
+        { field: 'status',headerName:"Status",cellClass:statusColumnClass },
+        { field: 'status',headerName:"Actions",cellClass:statusColumnClass },
     ])
     return (
         <Body title="Orders" className="h-full">
-            <div className="ag-theme-alpine" style={{height: "100%", width: "100%"}}>
+            <div className="ag-theme-alpine h-full w-full">
                 <AgGridReact
                     rowData={rowData}
-                    columnDefs={columnDefs}>
+                    columnDefs={columnDefs}
+                >
                 </AgGridReact>
             </div>
         </Body>
