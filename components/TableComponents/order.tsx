93d66193930d36debb8270b594cbc7e502550954
NatiGK <natnaelgashu2022@gmail.com>
2022-12-22 08:27:40 +0000

    include type in order table components

--- components/TableComponents/order.tsx
+++ components/TableComponents/order.tsx
@@ -1,6 +1,7 @@
+import { CellClassParams, ColDef, ICellRendererParams } from "ag-grid-community";
 import Button from "../UIElements/Button";
 
-export const OrderTableActionRow = (params:any)=>{
+export const OrderTableActionRow = (params: ICellRendererParams)=>{
     return(
         <div className="flex gap-4 font-semibold w-max">
             <Button type="outline" className="w-24">
@@ -22,17 +23,17 @@ export const OrderTableActionRow = (params:any)=>{
     );
 }
 
-const statusColumnClass = (param:any)=>(
+const statusColumnClass = (param:CellClassParams)=>(
     param?.value==="pending"?"text-yellow-600 text-lg":
     param?.value==="started"?"text-indigo-600 text-lg":
     param?.value==="served"?"text-green-600 text-lg":
     param?.value==="canceled"?"text-red-600 text-lg":
     "text-gray-600"
 );
-const headerClass = "text-gray-700 text-base";
-const cellClass = "text-gray-600 text-base";
+const headerClass:string = "text-gray-700 text-base";
+const cellClass:string = "text-gray-600 text-base";
 
-export const columnDefs = [
+export const columnDefs:ColDef[] = [
     { 
         field: '_id',
         headerName:"ID",
