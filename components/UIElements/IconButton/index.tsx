c5762f69de85e785eaaf8bd4ec482d00f2a3d8c8
NatiGK <natnaelgashu2022@gmail.com>
2022-12-22 12:55:45 +0000

    create icon button component

---
+++ components/UIElements/IconButton/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { IconType } from 'react-icons/lib';
+export interface TypeButton{
+    children: string,
+    size?:"sm"|"md"|"lg",
+    type?: "outline"|"fill"|"text",
+    color?: "primary"|"success"|"warning",
+    className?:string,
+    iconStart?:React.ReactNode,
+    iconEnd?:React.ReactNode,
+    onClick?:()=>void,
+}
+
+export default function IconButton({
+    children,
+    size="md",
+    type="fill",
+    color="primary",
+    className,
+    iconStart,
+    iconEnd,
+    onClick=()=>{}
+}: TypeButton){
+    return (
+        <button 
+            className={`
+                flex gap-2 items-center justify-center
+                rounded-lg transition-all ${className}
+                 m-1
+                ${size==="sm"?"text-xs p-1":size==="lg"&&"text-lg p-2"}
+                ${
+                    type==="outline"?`border 
+                        ${
+                            color==="primary"?"border-indigo-700 bg-indigo-700/10 text-indigo-700 hover:bg-indigo-700 hover:text-indigo-50":
+                            color==="warning"?"border-yellow-700 bg-yellow-700/10 text-yellow-700 hover:bg-yellow-700 hover:text-yellow-50":
+                            "border-green-700 bg-green-700/10 text-green-700 hover:bg-green-700 hover:text-green-50"
+                        }
+                    `:type==="fill"?`
+                        ${
+                            color==="primary"?"bg-indigo-500 text-indigo-50 hover:bg-indigo-700 hover:text-indigo-50":
+                            color==="warning"?"bg-yellow-500 text-yellow-50 hover:bg-yellow-700 hover:text-yellow-50":
+                            "bg-green-500 text-green-50 hover:bg-green-700 hover:text-green-50"
+                        }
+                    `:`bg-transparent
+                        ${
+                            color==="primary"?"text-indigo-700 hover:bg-indigo-700/20":
+                            color==="warning"?"text-yellow-700 hover:bg-yellow-700/20":
+                            "text-green-700 hover:bg-green-700/20"
+                        }
+                    `
+                }
+            `}
+            onClick={onClick}
+        >
+            <>
+                {iconStart}
+                {children}
+                {iconEnd}
+            </>
+        </button>
+    );
+}
