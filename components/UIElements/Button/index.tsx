6d8dfdd9410462797c59130c40f9dbe78f3aeebc
NatiG100 <natnaelgashu2022@gmail.com>
2022-12-23 07:55:04 -0800

    add error type option on button

--- components/UIElements/Button/index.tsx
+++ components/UIElements/Button/index.tsx
@@ -2,7 +2,7 @@ export interface TypeButton{
     children: string,
     size?:"sm"|"md"|"lg",
     type?: "outline"|"fill"|"text",
-    color?: "primary"|"success"|"warning",
+    color?: "primary"|"success"|"warning"|"error",
     className?:string,
     onClick?:()=>void,
 }
@@ -26,18 +26,21 @@ export default function Button({
                         ${
                             color==="primary"?"border-indigo-700 bg-indigo-700/10 text-indigo-700 hover:bg-indigo-700 hover:text-indigo-50":
                             color==="warning"?"border-yellow-700 bg-yellow-700/10 text-yellow-700 hover:bg-yellow-700 hover:text-yellow-50":
+                            color==="error"?"border-red-700 bg-red-700/10 text-red-700 hover:bg-red-700 hover:text-red-50":
                             "border-green-700 bg-green-700/10 text-green-700 hover:bg-green-700 hover:text-green-50"
                         }
                     `:type==="fill"?`
                         ${
                             color==="primary"?"bg-indigo-500 text-indigo-50 hover:bg-indigo-700 hover:text-indigo-50":
                             color==="warning"?"bg-yellow-500 text-yellow-50 hover:bg-yellow-700 hover:text-yellow-50":
+                            color==="error"?"bg-red-500 text-red-50 hover:bg-red-700 hover:text-red-50":
                             "bg-green-500 text-green-50 hover:bg-green-700 hover:text-green-50"
                         }
                     `:`bg-transparent
                         ${
                             color==="primary"?"text-indigo-700 hover:bg-indigo-700/20":
                             color==="warning"?"text-yellow-700 hover:bg-yellow-700/20":
+                            color==="error"?"text-red-700 hover:bg-red-700/20":
                             "text-green-700 hover:bg-green-700/20"
                         }
                     `
