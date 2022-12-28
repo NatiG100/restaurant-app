a5508b9cbf262001185d512b1c28da274e56f8dd
NatiGK <natnaelgashu2022@gmail.com>
2022-12-28 16:31:18 +0300

    fix from to hook

--- hooks/useFromTo.tsx
+++ hooks/useFromTo.tsx
@@ -1,30 +1,42 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useCallback, useState } from "react";
+import { formatDate } from "../utils/date";
 
 export default function useFromTo(){
-    const [from,setFrom] = useState<Date>(new Date());
-    const [to,setTo] = useState<Date>(new Date());
+    const [from,setFrom] = useState<string>(formatDate(new Date(Date.now())));
+    const [to,setTo] = useState<string>(formatDate(new Date(Date.now())));
     const [error,setError] = useState<string|null>(null);
     
-    const onFromChange = (event: React.ChangeEvent<HTMLDataElement>)=>{
-        const newFromDate = new Date(event.target.value);
-        if(newFromDate<=to){
-            setFrom(newFromDate);
-        }else{
+    const onFromChange = useCallback((event: ChangeEvent<HTMLDataElement>)=>{
+        const newFromDate = event.target.value;
+        if(newFromDate>to){
             setError("From should be before to.")
         }
-    }
-    const onToChange = (event: React.ChangeEvent<HTMLDataElement>)=>{
-        const newToDate = new Date(event.target.value);
-        if(newToDate>=from){
-            setTo(newToDate);
-        }else{
+        else if(formatDate(new Date(Date.now()))<newFromDate){
+            setError("From can't be after today.")
+        }
+        else{
+            setFrom(newFromDate);
+            setError(null);
+        }
+    },[to])
+    const onToChange = useCallback((event: ChangeEvent<HTMLDataElement>)=>{
+        const newToDate = event.target.value;
+        if(newToDate<from){
             setError("To should be after from.")
         }
-    }
-    [
+        else if(formatDate(new Date(Date.now()))<newToDate){
+            setError("To can't be after today.")
+        }
+        else{
+            setTo(newToDate);
+            setError(null);
+        }
+    },[from])
+    return {
         from,
         to,
+        error,
         onFromChange,
         onToChange,
-    ]
+    }
 }
