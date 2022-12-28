59dc6cf741aaa2c09c8bb7b323c81fd3c4156031
NatiGK <natnaelgashu2022@gmail.com>
2022-12-28 10:05:05 +0300

    create stat item

---
+++ components/stats/StatItem.tsx
@@ -0,0 +1,20 @@
+export interface TypeStatItem{
+    title:string,
+    value:number,
+    multiplier?:"K"|"M"|"B"|"",
+    fixTo?:number,
+    postfix?:string,
+}
+export default function StatItem ({fixTo=0,title,value,multiplier="",postfix=""}:TypeStatItem){
+    const divider = multiplier==="K"?1000:
+                    multiplier==="M"?1000000:
+                    multiplier==="B"?1000000000:1;
+    return(
+        <div className="flex flex-col gap-1">
+            <p className="text-gray-700 font-bold text-lg">{title}</p>
+            <div className="w-full px-2 py-2 flex items-center justify-start bg-gray-200 rounded-sm">
+                <p className="text-gray-700  text-lg">{(value/divider).toFixed(fixTo)+multiplier+" "+postfix}</p>
+            </div>
+        </div>
+    );
+}
--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,9 +1,16 @@
 import Body from "../components/Body";
+import StatItem from "../components/stats/StatItem";
 
 export default function Stats(){
     return (
     <Body title="Stats">
-      <p className="text-2xl">Stats</p>
+      <StatItem
+        title="Orders"
+        value={200010.124}
+        multiplier="K"
+        postfix="Birr"
+        fixTo={2}
+      />
     </Body>
     );
 }
