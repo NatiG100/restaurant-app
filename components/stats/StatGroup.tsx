69760da6cbe6b6d1c4c1de8a7e440d5425698089
NatiGK <natnaelgashu2022@gmail.com>
2022-12-28 10:37:24 +0300

    create order group

--- components/stats/StatGroup.tsx
+++ components/stats/StatGroup.tsx
@@ -1,13 +1,20 @@
-import { TypeStatItem } from "./StatItem";
+import Divider from "../UIElements/Divider";
+import StatItem, { TypeStatItem } from "./StatItem";
 
 export interface TypeStatItemGroup{
     title:string,
-    stats:TypeStatItem,
+    stats:TypeStatItem[],
 }
 export default function StatGroup (props:TypeStatItemGroup){
     return(
-        <div>
-            
+        <div className="flex flex-col gap-1 w-full">
+            <p>{props.title}</p>
+            <Divider/>
+            {
+                props.stats.map((stat)=>(
+                    <StatItem {...stat}/>
+                ))
+            }
         </div>
     );
 }
--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,15 +1,38 @@
 import Body from "../components/Body";
+import StatGroup from "../components/stats/StatGroup";
 import StatItem from "../components/stats/StatItem";
 
 export default function Stats(){
     return (
     <Body title="Stats">
-      <StatItem
-        title="Orders"
-        value={200010.124}
-        multiplier="K"
-        postfix="Birr"
-        fixTo={2}
+      <StatGroup
+        title="Order"
+        stats={[
+          {
+            title:"Total Orders",
+            value:2500,
+          },
+          {
+            title:"Orders/day (Avg)",
+            value:25,
+          },
+          {
+            title:"Total Food Orders",
+            value:2500,
+          },
+          {
+            title:"Food Orders/day (Avg)",
+            value:14,
+          },
+          {
+            title:"Total Drink Orders",
+            value:2500,
+          },
+          {
+            title:"Drink Orders/day (Avg)",
+            value:12,
+          },
+        ]}
       />
     </Body>
     );
