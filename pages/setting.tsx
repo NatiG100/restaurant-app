f4c3409b533bf684cbe1e0743b28c29cf00a0c02
NatiGK <natnaelgashu2022@gmail.com>
2022-12-29 10:55:42 +0300

    create setting page

--- pages/setting.tsx
+++ pages/setting.tsx
@@ -1,9 +1,37 @@
 import Body from "../components/Body";
+import IconButton from "../components/UIElements/IconButton";
+import LabledInput from "../components/UIElements/LabledInput";
+import {MdModeEdit as EditIcon} from 'react-icons/md'
 
 export default function Setting(){
     return (
         <Body title="Setting">
-      <p className="text-2xl">Setting</p>
-    </Body>
+          <div className="w-full  max-w-xl p-8 rounded-lg shadow-sm bg-white">
+            <div className="w-full max-w-sm flex flex-col gap-4">
+              <LabledInput
+                fullWidth
+                label={"Tax Rate"}
+                inputProps={{
+                  name:"taxRate",
+                  type:"number",
+                }}
+              />
+              <LabledInput
+                fullWidth
+                label={"Website Domain"}
+                inputProps={{
+                  name:"webDomain",
+                  type:"url",
+                }}
+              />
+              <IconButton
+                size="lg"
+                color="success"
+                className="w-full m-0 mt-8"
+                iconStart={<EditIcon/>}
+              >Edit Setting</IconButton>
+            </div>
+          </div>
+        </Body>
     );
 }
