bea12f3f032cb6385a1ad7bf352f39c3473f139e
NatiG100 <natnaelgashu2022@gmail.com>
2022-12-31 09:44:03 -0800

    add create food category modal on category table

--- pages/foods/categories.tsx
+++ pages/foods/categories.tsx
@@ -2,51 +2,43 @@ import {useRef, useEffect,useState, useCallback } from "react";
 import { AgGridReact } from "ag-grid-react";
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
-import {AiOutlinePrinter,AiOutlineExport} from 'react-icons/ai';
+import {GoPlus as PlusIcon} from 'react-icons/go';
 import IconButton from "../../components/UIElements/IconButton";
 import { columnDefs, defaultColDef, TypeFoodCategory } from "../../components/TableComponents/foodCategories";
 import { RowClickedEvent } from "ag-grid-community";
 import { useRouter } from "next/router";
 import Backdrop from "../../components/Backdrop";
-import FoodCategoriesModal from "../../components/modals/FoodCatagoriesModal";
+import FoodCategoriesModal from "../../components/modals/FoodCategoriesModal";
+import CreateFoodCategoryModal from "../../components/modals/CreateFoodCategoryModal";
 
 
 export default function DrinkCategories({setAppBarComponent}:any){
     const tableRef = useRef<HTMLDivElement>(null);
     // get ag-grid api ref
     const gridRef = useRef<AgGridReact>(null);
-    const handleExportClicked = ()=>{
-        gridRef.current!.api.exportDataAsCsv();
+
+    //state logic for create new category modal
+    const [openModal,setOpenModal] = useState<boolean>(false);
+    const handleCreateModalClose = () =>{
+        setOpenModal(false);
     }
-    const handlePrint = useCallback(()=>{
-        if(tableRef.current){
-            const api = gridRef.current!.api!;
-            api.setDomLayout("print");
-            setTimeout(function(){
-                print();
-                api.setDomLayout();
-            }, 2000)
-        }
-    },[tableRef])
+    const handleCreateModalOpen = () =>{
+        setOpenModal(true);
+    }
 
     //Add custom header components
     useEffect(()=>{
         setAppBarComponent(
           <div className="h-full flex gap-4 items-center">
+            
             <IconButton 
-                className="w-28 py-2" 
-                size="lg" 
-                iconStart={<AiOutlinePrinter className="text-xl"/>}
-                onClick={handlePrint}
-            >Print</IconButton>
-            <IconButton 
-                className="w-28 py-2" 
+                className="w-46 py-2" 
                 size="lg" 
                 type="outline"
                 color="success"
-                iconStart={<AiOutlineExport className="text-xl"/>}
-                onClick={handleExportClicked}
-            >Export</IconButton>
+                iconEnd={<PlusIcon className="text-xl"/>}
+                onClick={handleCreateModalOpen}
+            >Create New Category</IconButton>
           </div>
           
         );
@@ -105,6 +97,14 @@ export default function DrinkCategories({setAppBarComponent}:any){
                         <FoodCategoriesModal
                             onClose={handleModalClose}
                             category={selectedFoodCategory}
+                        />
+                    </Backdrop>
+                }
+                {
+                    openModal&&
+                    <Backdrop onClick={handleCreateModalClose}>
+                        <CreateFoodCategoryModal
+                            onClose={handleCreateModalClose}
                         />
                     </Backdrop>
                 }
