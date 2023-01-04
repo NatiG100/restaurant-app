0141e05fb368f7acef5240a26a4943bb371c44a7
NatiGK <natnaelgashu2022@gmail.com>
2023-01-04 15:02:55 +0300

    add permission list and edit view

---
+++ assets/permissions.ts
@@ -0,0 +1,13 @@
+export type TypePermission = "View Info"|"Manage Items"|"View Items"|
+    "View Users"|"Manage Users"|"Setting"|"Manage Orders"|"View Orders"
+
+export const allPermissions:TypePermission[] = [
+    "Manage Items",
+    "Manage Orders",
+    "Manage Users",
+    "Setting",
+    "View Info",
+    "View Items",
+    "View Orders",
+    "View Users",
+]
--- components/modals/userModals/AddUserModal.tsx
+++ components/modals/userModals/AddUserModal.tsx
@@ -3,13 +3,51 @@ import SingleImageUpload from "../../SingleImageUpload";
 import LabledInput from "../../UIElements/LabledInput";
 import BaseModal from "./../BaseModal";
 import {GoPlus as PlusIcon} from 'react-icons/go';
+import { allPermissions, TypePermission } from "../../../assets/permissions";
+import ToggleChip from "../../UIElements/ToggleChip";
 
 export interface TypeAddUserModal{
     onClose:()=>void
 }
 
+
+
 export default function AddUserModal({onClose}:TypeAddUserModal){
     const [userImage,setUserImage] = useState<string | null>(null);
+    const [selectedPermissions, setSelectedPermissions] = useState<TypePermission[]>([]);
+    const tooglePermission = (permission:TypePermission)=>()=>{
+
+        // if the permission already exists rmeove it
+        if(selectedPermissions.includes(permission)){
+            setSelectedPermissions((oldPermissions)=>{
+                let newPermissions:TypePermission[] = [...oldPermissions];
+                
+                //get the index of permission
+                const index:number = newPermissions.indexOf(permission);
+
+                //remove the permission from the list
+                newPermissions.splice(index,1);
+                return newPermissions;
+            })
+        }
+        // if the permission doesn't exist push it
+        else{
+            setSelectedPermissions((oldPermissions)=>{
+                const newPermission = [...oldPermissions];
+                newPermission.push(permission);
+                return newPermission;
+            })
+        }
+    }
+    const isPermissionOn = (permission:TypePermission)=>{
+        return selectedPermissions.includes(permission);
+    }
+    const classes = {
+        headerText:"text-lg font-bold text-gray-700",
+        text:"text-lg text-gray-500",
+        container:"grid grid-rows-maxmax w-full my-2",
+        twoCols:"grid grid-cols-1fr1fr",
+    }
     return(
         <BaseModal
             onClose={onClose}
@@ -25,7 +63,7 @@ export default function AddUserModal({onClose}:TypeAddUserModal){
                 <p className="text-xl font-bold text-indigo-700">Add User</p>
             }
         >
-            <div className="w-full">
+            <div className="w-full flex flex-col gap-2">
                 <SingleImageUpload
                     img={userImage}
                     setImg={setUserImage}
@@ -40,6 +78,22 @@ export default function AddUserModal({onClose}:TypeAddUserModal){
                     label="Email"
                     fullWidth
                 />
+            </div>
+            <div className={classes.container + " pt-4"}>
+                <p className={classes.headerText}>Permissions</p>
+                <div className="w-full pt-4 flex flex-wrap gap-2">
+                    {
+                        allPermissions.map((permission)=>(
+                            <ToggleChip
+                                key={permission}
+                                on={isPermissionOn(permission)}
+                                onToggle={tooglePermission(permission)}
+                            >
+                                {permission}
+                            </ToggleChip>
+                        ))
+                    }
+                </div>
             </div>
         </BaseModal>
     );
