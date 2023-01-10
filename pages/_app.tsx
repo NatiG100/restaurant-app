13ce2121e96f2b8cae6e9c391eda072a069b8f95
NatiGK <natnaelgashu2022@gmail.com>
2023-01-10 10:59:26 +0300

    remove layout for login page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,13 @@ import type { AppProps } from 'next/app'
 import Appbar from '../components/Appbar';
 import Navigation from '../components/Navigation';
 import Backdrop from '../components/Backdrop';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps){
   const [component,setComponent]= useState(<div></div>);
+  const router = useRouter();
+  console.log(router.pathname)
+  if(router.pathname==="/login") return <Component {...pageProps}/>
   return (
     <div className="grid grid-cols-main h-screen">
       <div id="portal" className='fixed z-10'></div>
