e6dfef58f288697adccd0936a7bd29bc4f8e3489
NatiGK <natnaelgashu2022@gmail.com>
2023-01-16 11:55:58 +0300

    add axios interceptors

--- services/AuthService.ts
+++ services/AuthService.ts
@@ -1,14 +1,9 @@
-import axios from "axios"
+import instance from "./instance";
 
 export const login = async (data:{email:string,password:string})=>{
-    return axios.post(
-        'http://localhost:4000/api/auth/login',
+    return instance.post(
+        'auth/login',
         data,
         {withCredentials:true}
-    ).then(data=>{
-        return data?.data;
-    })
-    .catch((error)=>{
-        return error?.response?.data;
-    });
+    );
 }
--- services/instance.tsx
+++ services/instance.tsx
@@ -1,6 +1,21 @@
 import axios from 'axios';
+
+
 const instance = axios.create({
-    baseURL: 'localhost:4000/api',
+    baseURL: 'http://localhost:4000/api/',
     timeout: 10000,
+});
+instance.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    return config;
+  }, function (error) {
+    return {message:"Client Error"};
+  });
+
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+    return response?.data;
+    }, function (error) {
+    return error?.response?.data
 });
 export default instance;
