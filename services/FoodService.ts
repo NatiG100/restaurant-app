31010260ea39498dd5e65f9c0b49d0305706e589
NatiGK <natnaelgashu2022@gmail.com>
2023-01-26 15:13:00 +0300

    create update food service

--- services/FoodService.ts
+++ services/FoodService.ts
@@ -28,23 +28,27 @@ export const addFood = async(data:TypeAddFood)=>{
     })
 }
 
-// //update food category service
-// export interface TypeUpdateFoodCategory{
-//     data:TypeAddFoodCategory,
-//     id:string
-// }
-// export const updateFoodCategory = async({data,id}:TypeUpdateFoodCategory)=>{
-//     //create form data and append fields
-//     const formData = new FormData();
-//     formData.append("name",data.name);
-//     formData.append("description",data.description);
-//     data.img&&formData.append("img",data.img,data.img.name);
-//     return instance.patch<TypeCustomeErrorResponse,TypeMultiDataResponse>(`/food-categories/${id}/update`,formData,{
-//         headers:{
-//             "Content-Type": "multipart/form-data",
-//         }
-//     })
-// }
+//update food category service
+export interface TypeUpdateFood{
+    name:string,
+    description:string,
+    cost:Number,
+    img:File|null,
+    id:string
+}
+export const updateFood = async({name,description,cost,img,id}:TypeUpdateFood)=>{
+    //create form data and append fields
+    const formData = new FormData();
+    formData.append("name",name);
+    formData.append("description",description);
+    formData.append("cost",cost.toString())
+    img&&formData.append("img",img,img.name);
+    return instance.patch<TypeCustomeErrorResponse,TypeMultiDataResponse>(`/food/${id}/update`,formData,{
+        headers:{
+            "Content-Type": "multipart/form-data",
+        }
+    })
+}
 
 // //change food category status service
 // export interface TypeChangeFoodCategoryStatus{
