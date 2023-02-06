bb6ad167a23d1f9451985df12a3d0ab63e45b782
NatiG100 <natnaelgashu2022@gmail.com>
2023-02-06 13:29:35 +0300

    create skeleton for food category list

--- pages/client/foods/index.tsx
+++ pages/client/foods/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
 import { useQuery } from "react-query";
 import { toast } from "react-toastify";
 import Category from "../../../components/client/Category";
 import baseURL from "../../../constants/BASE_URL";
 import { fetchAllFoodCategories } from "../../../services/ClientServices";
 import { TypeCustomeErrorResponse, TypeMultiDataResponse } from "../../../types/types";
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function Foods(){
     const {data,error} = useQuery<
@@ -21,7 +23,7 @@ export default function Foods(){
     
     return(
         <div>
-            <>
+            {data?.data?<>
                 <Category title="All" img="/dessert.jpg" numberOfItems={44}/>
                 {data?.data.map((foodCategory)=>(
                     <Category 
@@ -33,7 +35,26 @@ export default function Foods(){
                         key={foodCategory.id}
                     />
                 ))}            
-            </>
+            </>:
+            <div className="py-4 px-3">
+                <div className="grid grid-cols-mx1fr gap-4 items-center border border-gray-100 rounded-xl p-2 my-4">
+                    <Skeleton circle width={70} height={70}/>
+                    <Skeleton height={20} count={2}/>
+                </div>
+                <div className="grid grid-cols-mx1fr gap-4 items-center border border-gray-100 rounded-xl p-2 my-4">
+                    <Skeleton circle width={70} height={70}/>
+                    <Skeleton height={20} count={2}/>
+                </div>
+                <div className="grid grid-cols-mx1fr gap-4 items-center border border-gray-100 rounded-xl p-2 my-4">
+                    <Skeleton circle width={70} height={70}/>
+                    <Skeleton height={20} count={2}/>
+                </div>
+                <div className="grid grid-cols-mx1fr gap-4 items-center border border-gray-100 rounded-xl p-2 my-4">
+                    <Skeleton circle width={70} height={70}/>
+                    <Skeleton height={20} count={2}/>
+                </div>
+            </div>
+            }
         </div>
     );
 };
