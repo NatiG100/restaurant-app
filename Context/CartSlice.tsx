d06afeaae17e531e9e568d38bd1be253f4d53bca
NatiG100 <natnaelgashu2022@gmail.com>
2023-02-13 09:52:26 +0300

    create subtract food reducer

--- Context/CartSlice.tsx
+++ Context/CartSlice.tsx
@@ -17,21 +17,47 @@ export const CartSlice = createSlice({
     initialState,
     reducers:{
         addFood:(state,action:PayloadAction<TypeFood>)=>{
+            //assume the food is not in the state and set food posititon to -1
             let foodPos = -1;
             state.foods.forEach((food,index)=>{
+                //if the food is in the state store it's index in food position
+                if(food.food.id===action.payload.id){
+                    foodPos = index;
+                }
+            });
+
+            let foods = [...state.foods];
+            //if the food was found increment it's quantity
+            if(foodPos!==-1){
+                foods[foodPos].count +=1;
+            }
+            //if the food was not found push it
+            else{
+                let foods = [...state.foods];
+                foods.push({food:action.payload,count:1});
+            }
+            //update the state including the total cost
+            let newState={...state,foods,totalCost:state.totalCost+action.payload.cost}
+            return newState;
+        },
+        subtractFood:(state,action:PayloadAction<TypeFood>)=>{
+            //assume the food is not in the state and set food posititon to -1
+            let foodPos = -1;
+            state.foods.forEach((food,index)=>{
+                //if the food is in the state store it's index in food position
                 if(food.food.id===action.payload.id){
                     foodPos = index;
                 }
             });
             let foods = [...state.foods];
-            if(foodPos!==-1){
-                foods[foodPos].count +=1;
-            }else{
-                let foods = [...state.foods];
-                foods.push({food:action.payload,count:1});
-            }
-            let newState={...state,foods,totalCost:state.totalCost+state.foods[foodPos].food.cost}
+            //decrement the food quantity
+            foods[foodPos].count -=1;
+            //if the quantity is 0 remove it from the array
+            if(foods[foodPos].count===0) {foods.splice(foodPos,foodPos)};
+
+            //update the state including the total cost
+            let newState={...state,foods,totalCost:state.totalCost-action.payload.cost}
             return newState;
-        }
+        },
     }
 })
