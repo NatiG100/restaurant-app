ca9cf8fb47037676dd4ca4d442cd6df0387176b6
NatiGK <natnaelgashu2022@gmail.com>
2023-03-03 15:46:47 +0300

    add setting feature

--- pages/setting.tsx
+++ pages/setting.tsx
@@ -2,36 +2,86 @@ import Body from "../components/Body";
 import IconButton from "../components/UIElements/IconButton";
 import LabledInput from "../components/UIElements/LabledInput";
 import {MdModeEdit as EditIcon} from 'react-icons/md'
+import { useMutation, useQuery } from "react-query";
+import { fetchSetting, TypeFetchSettingResponse, TypeUpdateSetting, updateSetting } from "../services/SettingService";
+import { TypeCustomeErrorResponse, TypeMultiDataResponse } from "../types/types";
+import Loading from "../components/UIElements/Loading";
+import {useEffect} from 'react'
+import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
 
 export default function Setting(){
+    const {data,error,isLoading} = useQuery<
+      TypeFetchSettingResponse,
+      TypeCustomeErrorResponse
+    >('fetchApplicationSetting',fetchSetting);
+
+    useEffect(()=>{
+      if(error){
+        toast(error.message,{type:"error"});
+      }
+      else if(data){
+        setValue("frontendWebDomain",data.data.frontendWebDomain);
+        setValue("taxRate",data.data.taxRate);
+      }
+    },[data,error]);
+    const {handleSubmit,formState:{errors},register,setValue} = useForm<TypeUpdateSetting>();
+    
+    const {
+      mutate,
+      error:updateError,
+      data:updateData,
+      isLoading:isUpdateLoading
+    } = useMutation<
+      TypeMultiDataResponse,
+      TypeCustomeErrorResponse,
+      TypeUpdateSetting
+    >(updateSetting);
+    useEffect(()=>{
+      if(updateError){
+        toast(updateError.message,{type:"error"});
+      }
+      else if(data){
+        toast(updateData?.message,{type:"success"});
+      }
+    },[updateError,updateData]);
+    const onSubmit = (data:TypeUpdateSetting)=>{
+      mutate(data);
+    }
+    if(isLoading) return <Loading type="full"/>
     return (
         <Body title="Setting">
           <div className="w-full  max-w-xl p-8 rounded-lg shadow-sm bg-white">
             <div className="w-full max-w-sm flex flex-col gap-4">
-              <LabledInput
-                fullWidth
-                label={"Tax Rate"}
-                inputProps={{
-                  placeholder:"Enter Tax Rate (%)",
-                  name:"taxRate",
-                  type:"number",
-                }}
-              />
-              <LabledInput
-                fullWidth
-                label={"Website Domain"}
-                inputProps={{
-                  placeholder:"Enter your Website url",
-                  name:"webDomain",
-                  type:"url",
-                }}
-              />
-              <IconButton
-                size="lg"
-                color="success"
-                className="w-full m-0 mt-8"
-                iconStart={<EditIcon/>}
-              >Edit Setting</IconButton>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <LabledInput
+                  fullWidth
+                  label={"Tax Rate"}
+                  inputProps={{
+                    placeholder:"Enter Tax Rate (%)",
+                    type:"number",
+                    ...register("taxRate",{required:"Tax rate is required"})
+                  }}
+                  error={errors.taxRate?.message}
+                />
+                <LabledInput
+                  fullWidth
+                  label={"Website Domain"}
+                  inputProps={{
+                    placeholder:"Enter your Website url",
+                    type:"url",
+                    ...register("frontendWebDomain",{required:"Website domain is required"})
+                  }}
+                  error={errors.frontendWebDomain?.message}
+                />
+                <IconButton
+                  size="lg"
+                  color="success"
+                  className="w-full m-0 mt-8"
+                  iconStart={<EditIcon/>}
+                  butonProps={{disabled:isUpdateLoading}}
+                >Edit Setting</IconButton>
+              </form>
             </div>
           </div>
         </Body>
---
+++ services/SettingService.ts
@@ -0,0 +1,22 @@
+import { TypeCustomeErrorResponse, TypeMultiDataResponse } from "../types/types";
+import instance from "./instance";
+export interface TypeFetchSettingResponse{
+    data:TypeUpdateSetting,
+}
+export const fetchSetting = async ()=>{
+    return instance.get<
+        TypeCustomeErrorResponse,
+        TypeFetchSettingResponse
+    >(`/setting`)
+}
+export interface TypeUpdateSetting{
+    taxRate:number,
+    frontendWebDomain:string,
+}
+export const updateSetting = async(data:TypeUpdateSetting)=>{
+    console.log(data);
+    return instance.patch<
+        TypeCustomeErrorResponse,
+        TypeMultiDataResponse
+    >(`/setting`,data);
+}
