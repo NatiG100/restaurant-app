641352dbf6aedd35d458894251a3061d620744fb
NatiG100 <natnaelgashu2022@gmail.com>
2023-03-14 14:57:09 +0300

    improve animate on unmount hook

--- hooks/useAnimateOnWillUnmout.tsx
+++ hooks/useAnimateOnWillUnmout.tsx
@@ -1,23 +1,13 @@
 import {useState,useCallback,useEffect} from 'react';
-export default function(initial:boolean){
+export default function(initial:boolean,showState?:boolean){
     const [show,setShow] = useState(initial);
     const [shouldRender,setShouldRender] = useState(initial);
-    const triggerRender = useCallback(()=>{
-        setShow(true);
-    },[]);
-    const triggerRemove = useCallback(()=>{
-        setShow(false);
-    },[]);
-    useEffect(()=>{
-        if(show){
-            setShouldRender(true);
-        }
-    },[show]);
-    const onAnimationEnd = useCallback(()=>{
-        if(!show){
-            setShouldRender(false);
-        }
-    },[show]);
+    //if show state was provided use that to change 'show'
+    useEffect(()=>{if(showState!==null&&showState!==undefined)setShow(showState);},[showState])
+    const triggerRender = useCallback(()=>{setShow(true);},[]);
+    const triggerRemove = useCallback(()=>{setShow(false);},[]);
+    useEffect(()=>{if(show)setShouldRender(true);},[show]);
+    const onAnimationEnd = useCallback(()=>{if(!show){setShouldRender(false);}},[show]);
 
     return{
         show,
