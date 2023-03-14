6f5758bb4930416d450613b545db470450099fab
NatiG100 <natnaelgashu2022@gmail.com>
2023-03-14 10:04:54 +0300

    improve outside click listener hook

--- hooks/useOutsideClickListner.tsx
+++ hooks/useOutsideClickListner.tsx
@@ -3,12 +3,29 @@ import { useState, useEffect, MouseEventHandler } from "react";
 /**
  * Hook that alerts clicks outside of the passed ref
  */
-export function useOutsideClickListner(ref:React.RefObject<HTMLElement>) {
-    const [clickedOutside, setClickedOutside] = useState(false);
+export function useOutsideClickListner(
+        ref:React.RefObject<HTMLElement>,
+        exceptionElements:React.RefObject<HTMLElement>[]=[],
+        initial=true
+    ) {
+    const [clickedOutside, setClickedOutside] = useState(initial);
     useEffect(() => {
         function handleClickOutside(event:any) {
             if (ref.current && !ref.current.contains(event.target)) {
                 setClickedOutside(true);
+            }
+            else{  
+                let exceptionElementClicked = false;
+                exceptionElements.forEach((exceptionElement)=>{
+                    if(event.target===exceptionElement.current){
+                        exceptionElementClicked = true;
+                    }
+                });
+                if(exceptionElementClicked){
+                    setClickedOutside((prev)=>(!prev));
+                }else{
+                    setClickedOutside(false);
+                }
             }
         }
 
