3ef366d439f885eb4055da3f9539af4c43c5ec01
NatiG100 <natnaelgashu2022@gmail.com>
2023-03-19 12:07:40 +0300

    add conditional redirection in Redirect hoc

--- components/hoc/Redirec.tsx
+++ components/hoc/Redirec.tsx
@@ -2,19 +2,28 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../Context/store";
+import { TypeUser } from "../TableComponents/user";
 import Loading from "../UIElements/Loading";
 
 export default function Redirect({children}:{children:React.ReactElement}){
     const router = useRouter();
-    const user = useSelector<RootState>(state=>state?.auth?.user);
+    const user:TypeUser = useSelector<RootState>(state=>state?.auth?.user) as TypeUser;
     const [loading, setLoading]= useState(true);
     useEffect(()=>{
         if(user){
-            router.replace('/').then(()=>{
-                setTimeout(()=>{
-                    setLoading(false);
-                },500);
-            })
+            if(user.previlages.includes("View Info")){
+                router.replace('/').then(()=>{
+                    setTimeout(()=>{
+                        setLoading(false);
+                    },500);
+                });
+            }else if(router.pathname!=="/foods/categories"){
+                router.replace('/foods/categories').then(()=>{
+                    setTimeout(()=>{
+                        setLoading(false);
+                    },500);
+                });  
+            }
         }else if(!user){
             router.replace('/login').then(()=>{
                 setTimeout(()=>{
