afff2fed9ca6b41545220af17de6ee64dabeaeb1
NatiGK <natnaelgashu2022@gmail.com>
2023-03-23 13:59:50 +0300

    fix notification logic

--- components/Appbar/Notificatin.tsx
+++ components/Appbar/Notificatin.tsx
@@ -4,9 +4,26 @@ import { useOutsideClickListner } from "../../hooks/useOutsideClickListner";
 import IconButton from "../UIElements/IconButton";
 import {IoNotificationsSharp as NotificationIcon} from 'react-icons/io5';
 import { socket } from "../../utils/socket";
+import { useMutation, useQuery } from "react-query";
+import { fetchAllNotifications, FetchNotificationError, FetchNotificationsSuccess, seeAllNotifications } from "../../services/NotificationServices";
+import Loading from "../UIElements/Loading";
 
+type TypeNotification = {
+    title:string,
+    description:string,
+    seen:boolean,
+    _id:string
+}
 
 export default function Notification (){
+    const {
+        data:notifications,
+        error:notificationError,
+        refetch:refetchNotifications,
+        isLoading:notificationsLoading,
+    } = useQuery<FetchNotificationsSuccess,FetchNotificationError>('fetchNotifications',fetchAllNotifications);
+
+
     const notificationRef = useRef(null);
     const notificationButtonRef = useRef(null);
     const clickedOutsideNotificationButton = useOutsideClickListner(notificationRef,[notificationButtonRef]);
@@ -25,21 +42,31 @@ export default function Notification (){
             setSeen(false);
             console.log(data);
         }
-        function onConnect(){
-            console.log("connected")
-        }
         socket.on('notification',onEvent);
-        socket.on('connect',onConnect)
-        return()=>{
-            socket.off("notification",onEvent);
-            socket.off("event",onConnect)
-        }
+        return()=>{socket.off("notification",onEvent);}
     },[]);
+
+    //make all notifications seen when the user opens the notification list
+    const {mutate} = useMutation(seeAllNotifications);
     useEffect(()=>{
         if(shouldRenderNotifications){
-            setSeen(true);
+            mutate();
+        }else{
+            refetchNotifications();
         }
-    },[shouldRenderNotifications])
+    },[shouldRenderNotifications]);
+
+    useEffect(()=>{
+        if(notifications?.data){
+            let tempSeen = true;
+            notifications.data.map((notification)=>{
+                if(!notification.seen){
+                    tempSeen = false;
+                }
+            });
+            setSeen(tempSeen);
+        }
+    },[notifications]);
 
     return(
         <div className="relative" ref={notificationRef}>
@@ -52,12 +79,24 @@ export default function Notification (){
             > </IconButton>
             {shouldRenderNotifications&&<div onAnimationEnd={onNotificationAnimationEnd} className=
                 {`absolute top-full right-0
-                bg-gray-50 rounded-lg shadow-sm p-3 py-4 gap-4
-                z-40 border border-gray-300 ${showNotification?'animate-zoom-fade-in':'animate-zood-fade-out'}`}
+                bg-gray-50 rounded-lg shadow-sm p-3 py-4 gap-2
+                z-40 border border-gray-300 ${showNotification?'animate-zoom-fade-in':'animate-zood-fade-out'}
+                    max-h-80 overflow-x-auto scrollbar-hide flex flex-col shadow-lg
+                `}
             >
-                <SingleNotification title="New Order" description="a new order has arrived from a client" seen={false}/>
-                <SingleNotification title="New Order" description="a new order has arrived from a client" seen={false}/>
-                <SingleNotification title="New Order" description="a new order has arrived from a client" seen={true}/>
+                {notificationsLoading?<Loading type="contained"/>:
+                    notifications?.data.map((notification)=>(
+                        <SingleNotification 
+                            title={notification.title}
+                            description={notification.description} 
+                            seen={notification.seen}
+                            key={notification._id}
+                        />
+                    ))
+                }
+                {
+                    notificationError&&<p className="text-red-500 text-sm w-40">Error while trying to load notifications</p>
+                }
             </div>}
         </div>
     );
@@ -71,8 +110,8 @@ export interface SingleNotificationProps{
 function SingleNotification({title,description,seen}:SingleNotificationProps){
     return(
         <div className={`
-            grid grid-cols-mx1fr  p-2 py-1 cursor-default
-            gap-7 items-center rounded-sm ${!seen&&"bg-indigo-400/5"}
+            grid grid-cols-mx1fr  p-3 py-2 cursor-default
+            gap-7 items-center rounded-lg ${seen?"bg-gray-400/5":"bg-indigo-400/5"}
         `}>
             <div className="
                 w-48 flex flex-col
