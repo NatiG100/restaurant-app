c8b2cb6288286a8304b5ff38302cf4391a6b966b
NatiGK <natnaelgashu2022@gmail.com>
2023-03-23 10:33:18 +0300

    create notification services

---
+++ services/NotificationServices.ts
@@ -0,0 +1,15 @@
+import instance from "./instance"
+
+export interface FetchNotificationsSuccess{
+    data:[{title:string,description:string,seen:boolean,_id:string}]
+}
+export interface FetchNotificationError{
+    message:string,
+}
+export const fetchAllNotifications = async()=>{
+    return instance.get<FetchNotificationError,FetchNotificationsSuccess>('/notifications');
+}
+
+export const seeAllNotifications = async()=>{
+    return instance.post<{message:string},{message:string}>('/notifications/see');
+}
