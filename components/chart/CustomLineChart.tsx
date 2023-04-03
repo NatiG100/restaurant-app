e6fb79236194aa5bf7942a594a243cfa4937f013
NatiGK <natnaelgashu2022@gmail.com>
2023-04-03 14:05:23 +0300

    fix custom line chart

--- components/chart/CustomLineChart.tsx
+++ components/chart/CustomLineChart.tsx
@@ -1,16 +1,14 @@
-import { useEffect } from "react";
+import { useEffect,Fragment } from "react";
 import { VictoryAxis, VictoryLabel, VictoryLine, VictoryScatter, VictoryTooltip } from "victory";
 import { DOtW, MOtY } from "../../constants/constants";
 import ResponsiveVictoryChart from "../ResponsiveVictoryChart";
 
-export default function CustomLineChart ({data,color,selectedOption}:{data:{amount:number,date:string}[],color:string,selectedOption:number}){
-    useEffect(()=>{console.log(data)},[data]);
-    return(<>
+export default function CustomLineChart ({datas,colors,selectedOption}:{datas:{amount:number,date:string}[][],colors:string[],selectedOption:number}){
+    return(
     <ResponsiveVictoryChart
         padding={{ top: 0, bottom: 20, right: 50, left: 70 }}
         domainPadding={30} 
     >
-
         <VictoryAxis
             style={{
                 tickLabels:{fill:"rgb(55 65 81)"},
@@ -20,14 +18,14 @@ export default function CustomLineChart ({data,color,selectedOption}:{data:{amou
                 if(selectedOption===2||selectedOption===4){
                     return x;
                 }else if(selectedOption===1){
-                    const dates = data.map((d)=>(d.date));
+                    const dates = datas[1].map((d)=>(d.date));
                     return DOtW[dates.indexOf(x)]
                 }else{
                     return MOtY[parseInt(x)-1];
                 }
             }}
         />
-
+    
         <VictoryAxis     
             dependentAxis
             label={"Orders"}
@@ -46,67 +44,81 @@ export default function CustomLineChart ({data,color,selectedOption}:{data:{amou
                 <VictoryLabel dy={-28} />
             }
         />
-
-        <VictoryLine
-            data={data}
-            x="date"
-            y="amount"
-            style={{
-                data:{
-                    stroke:color+"22",
-                    strokeLinecap:"round",
-                    strokeWidth:"8px"
-                },
-                labels:{
-                    fill:"rgb(192 38 211)",
-                    fontSize:"15px",
-                    fontWeight:"600"
-                }
-            }}
-        />
-        <VictoryLine
-            data={data}
-            x="date"
-            y="amount"
-            style={{
-                data:{
-                    stroke:"rgb(192 38 211)",
-                },
-            }}
-        />
-        <VictoryScatter
-            labels={({datum})=>datum.value}
-            labelComponent={
-                <VictoryTooltip
-                    
-                    flyoutWidth={95}
-                    flyoutHeight={35}
-                    cornerRadius={5}
-                    pointerLength={40}
-                    flyoutStyle={{
-                    stroke: "rgb(192 38 211)",
-                    strokeWidth: 2,
-                    fill: "#FFFFFF"
-                    }}
+        {datas.map((data,index)=>{
+            let color = colors[index];
+            return(
+                <VictoryLine
+                    data={data}
+                    x="date"
+                    y="amount"
                     style={{
-                    fill: "rgb(192 38 211)",
-                    fontSize: 14,
-                    fontWeight: 500,
-                    textAnchor: "middle"
+                        data:{
+                            stroke:color+"22",
+                            strokeLinecap:"round",
+                            strokeWidth:"8px"
+                        },
+                        labels:{
+                            fill:color,
+                            fontSize:"15px",
+                            fontWeight:"600"
+                        }
                     }}
                 />
-            }
-            data={data}
-            x="date"
-            y="amount"
-            size={5}
-            style={{
-                data:{
-                    fill:color,
-                }
-            }}
-            
-        />
+            ) 
+        })}
+        {datas.map((data,index)=>{
+            let color = colors[index];
+            return(
+                <VictoryLine
+                    data={data}
+                    x="date"
+                    y="amount"
+                    style={{
+                        data:{
+                            stroke:color,
+                        },
+                    }}
+                />       
+            )
+        })}
+        {datas.map((data,index)=>{
+            let color = colors[index];
+            return(
+                <VictoryScatter
+                    labels={({datum})=>datum.value}
+                    labelComponent={
+                        <VictoryTooltip
+                            
+                            flyoutWidth={95}
+                            flyoutHeight={35}
+                            cornerRadius={5}
+                            pointerLength={40}
+                            flyoutStyle={{
+                            stroke: color,
+                            strokeWidth: 2,
+                            fill: "#FFFFFF"
+                            }}
+                            style={{
+                            fill: color,
+                            fontSize: 14,
+                            fontWeight: 500,
+                            textAnchor: "middle"
+                            }}
+                        />
+                    }
+                    data={data}
+                    x="date"
+                    y="amount"
+                    size={5}
+                    style={{
+                        data:{
+                            fill:color,
+                        }
+                    }}
+                    
+                />
+            );
+        })}
     </ResponsiveVictoryChart>
-    </>);
+    );
 }
