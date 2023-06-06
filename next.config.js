d200d6e7565e2401eec9c08f701621d9277463ed
NatiGK <natnaelgashu2022@gmail.com>
2023-06-06 11:33:40 +0300

    correct header domain

--- next.config.js
+++ next.config.js
@@ -37,32 +37,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 'localhost',
-        port: '4000',
-        pathname: '/img/**',
-      },
-      {
-        protocol: 'http',
-        hostname: "172.20.44.116",
-        port: '4000',
-        pathname: '/img/**',
-      },
-      {
-        protocol: 'http',
-        hostname: "192.168.1.11",
-        port: '4000',
-        pathname: '/img/**',
-      },
-      {
-        protocol: 'http',
-        hostname: "172.20.101.37",
-        port: '4000',
-        pathname: '/img/**',
-      },
-      {
-        protocol: 'http',
-        hostname: "192.168.1.10",
-        port: '4000',
+        hostname: 'restaurant-app-api-kkdj.onrender.com',
+        port: '10000',
         pathname: '/img/**',
       },
     ],
--- services/AuthService.ts
+++ services/AuthService.ts
@@ -5,20 +5,20 @@ export const login = async (data:{email:string,password:string})=>{
     return instance.post<TypeCustomeErrorResponse,TypeMultiDataResponse>(
         'auth/login',
         data,
-        {withCredentials:true,headers:{"access-control-allow-origin":"restaurant-app-api-kkdj.onrender.com"}},
+        {withCredentials:true,headers:{"access-control-allow-origin":".onrender.com"}},
     );
 };
 
 export const logout = async ()=>{
     return instance.post(
         'auth/logout',
-        {withCredentials:true,headers:{"access-control-allow-origin":"restaurant-app-api-kkdj.onrender.com"}}
+        {withCredentials:true,headers:{"access-control-allow-origin":".onrender.com"}}
     );
 };
 
 export const who_am_i = async()=>{
     return instance.get(
         'auth/who-am-i',
-        {withCredentials:true,headers:{"access-control-allow-origin":"restaurant-app-api-kkdj.onrender.com"}}
+        {withCredentials:true,headers:{"access-control-allow-origin":".onrender.com"}}
     );
 };
