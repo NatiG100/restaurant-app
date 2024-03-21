fdf75fa8a6cef9769f54b6d01faa2f04bad3ebad
Natnael Ggashu <87175869+NatiG100@users.noreply.github.com>
2024-03-21 15:00:35 +0300

    Update BASE_URL.ts

--- constants/BASE_URL.ts
+++ constants/BASE_URL.ts
@@ -1,3 +1,3 @@
-// const baseURL = 'https://restaurant-app-api-kkdj.onrender.com';
-const baseURL = 'http://localhost:4000';
+const baseURL = 'https://restaurant-app-api-kkdj.onrender.com';
+// const baseURL = 'http://localhost:4000';
 export default baseURL;
